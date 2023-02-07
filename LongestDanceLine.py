d545a6f3399e5c32fbc4e3ff81805ad8718a8b4c
Mariana Pulido <109924753+lpulidoa@users.noreply.github.com>
2023-02-07 09:56:28 -0500

    Add files via upload

---
+++ LongestDanceLine.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+file = 'DanceFloor02.txt'
+
+A=np.loadtxt(file, skiprows=1, dtype=int)
+A.astype(int)
+n=len(A)
+caminos=np.zeros((n,n))
+
+m=0
+for i in range(n):
+	
+	for j in range(n):
+		n1=1
+		n2=1
+		if(i>0):
+			if(np.absolute(int(A[i][j])-int(A[i-1][j]))==1):
+				n1=caminos[i-1][j]+1
+		if(j>0):
+			if(np.absolute(int(A[i][j])-int(A[i][j-1]))==1):
+				n2=caminos[i][j-1]+1
+		if(n1>n2):
+				caminos[i][j]=n1
+		else:
+				caminos[i][j]=n2
+		if(caminos[i][j]>m):
+			m=caminos[i][j]
+
+print(A)						
+print(caminos)
+print(m)
+x=np.where(caminos==m)
+print(x)
