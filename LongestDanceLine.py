4b0d55e692db29177f83f22b0776697adf5680cd
Mariana Pulido <109924753+lpulidoa@users.noreply.github.com>
2023-02-08 11:42:39 -0500

    Update LongestDanceLine.py

--- LongestDanceLine.py
+++ LongestDanceLine.py
@@ -1,11 +1,7 @@
 import numpy as np
 
-example = 'DanceFloor01.txt'
-danceFloor1=np.loadtxt(example, skiprows=1, dtype=int)
-
-
-def findPaths( danceFloor ):
-    n = len(danceFloor)
+def findPaths( dance_floor ):
+    n = len(dance_floor)
     paths = np.zeros((n,n))
 
     for row in range(n):
@@ -13,10 +9,10 @@ def findPaths( danceFloor ):
             stepsUp = 1
             stepsLeft = 1
             if(row>0):
-                if(np.absolute(int(danceFloor[row][column])-int(danceFloor[row-1][column]))==1):
+                if(np.absolute(int(dance_floor[row][column])-int(dance_floor[row-1][column]))==1):
                     stepsUp = paths[row-1][column]+1
             if(column>0):
-                if(np.absolute(int(danceFloor[row][column])-int(danceFloor[row][column-1]))==1):
+                if(np.absolute(int(dance_floor[row][column])-int(dance_floor[row][column-1]))==1):
                     stepsLeft = paths[row][column-1]+1
             if(stepsUp>stepsLeft):
                     paths[row][column] = stepsUp
@@ -26,38 +22,43 @@ def findPaths( danceFloor ):
     return paths 
 
 
-def buildLongestPath( paths , danceFloor ):
+def buildLongestPath( dance_floor , paths ):
 
     lenght = int(paths.max())
-    index = np.where( paths==lenght )
-    i1 = index[0][0]
-    i2 = index[1][0]
-    rec=np.zeros(lenght)
-    rec[0] = danceFloor[i1][i2]
+    last_step = np.where( paths==lenght )
+    row = last_step[0][0]
+    column = last_step[1][0]
+    longest_path = np.zeros(lenght)
+    longest_path[0] = dance_floor[row][column]
 
-    for i in range(lenght-1):
-        if(i2!=0):
-            if(np.absolute(int(danceFloor[i1][i2])-int(danceFloor[i1][i2-1]))==1 and int(paths[i1][i2])-int(paths[i1][i2-1])==1):
-                i2 = i2-1
+    for step in range(lenght-1):
+        if(column!=0):
+            if(np.absolute(int(dance_floor[row][column])-int(dance_floor[row][column-1]))==1 and int(paths[row][column])-int(paths[row][column-1])==1):
+                column-=1
             else:
-                i1 = i1-1
+                row-=1
         else:
-            i1 = i1-1
+            row-=1
         
-        rec[i+1] = danceFloor[i1][i2]
+        longest_path[step+1] = dance_floor[row][column]
 
-
-    longestPath= np.flip(rec)
-    longestPath = [int(steps) for steps in longestPath]
+    longest_path = np.flip(longest_path)
     
-    return longestPath
+    return longest_path
 
-def PathString( path ):
-	pathString = ""
+def pathToString( path ):
+	path_string = ""
 	for step in path:
-		pathString += str(int(step)) + "-"
-	pathString = pathString[:-1]	
-	return pathString
+		path_string += str(int(step)) + " - "
+	path_string = path_string[:-2]	
+	return path_string
 
-print( int(findPaths(danceFloor1).max()) )
-print( PathString(buildLongestPath( findPaths(danceFloor1), danceFloor1 ) ) )
+
+example = 'DanceFloor01.txt'
+danceFloor = np.loadtxt(example, skiprows=1, dtype=int)
+
+path_lenght = int(findPaths(danceFloor).max())
+winner_path = pathToString( buildLongestPath( danceFloor , findPaths(danceFloor) ) )
+
+print("Longest Endavans Line Dance is:", winner_path )
+print("Length of Path is:", path_lenght )
